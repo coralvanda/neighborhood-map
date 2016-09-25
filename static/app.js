2bff77904f8031245c77d6463666179d7c2ad5b1
Eric <coralvanda@gmail.com>
2016-09-25 13:40:18 +0200

    Removed asyncronous loading of map, put markers on the map

--- static/app.js
+++ static/app.js
@@ -1,37 +1,59 @@
 // Google Map API key AIzaSyCz4KE50vlj_IUxdao2tVokcSaN8GJIdb4
 
-var map;
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
+var map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: -34.397, lng: 150.644},
 	  zoom: 8
-	});
+});
 
-	var input = document.getElementById('pac-input');
-    //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
+var input = document.getElementById('pac-input');
+//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+var autocomplete = new google.maps.places.Autocomplete(input);
 
 
-/**
-var model = {
-	null;
-}
+
+
+
+
+var myList = [
+    {
+    	position: {lat: -34.395, lng: 150.645},
+    	map: map,
+    	title: "A"
+    },
+    {
+    	position: {lat: -34.398, lng: 150.648},
+    	map: map,
+    	title: "B"
+    },
+    {
+    	position: {lat: -34.395, lng: 150.648},
+    	map: map,
+    	title: "C"
+    }
+]
 
 
 var viewModel = {
 	init: function() {
-		model.init();
-		view.init();
+	    view.populateMarkers();
+	},
+
+	buildList: function(itemList) {
+	var self = this;
+	this.placeList = ko.observableArray(itemList);
 	}
 }
 
 
 var view = {
-	null;
-}
+	populateMarkers: function() {
+		myList.forEach(function(location) {
+			var marker = new google.maps.Marker(location);
+			marker.setMap(map);
+		})
+	}
+};
 
 
+ko.applyBindings(new viewModel.buildList(myList));
 viewModel.init();
-**/
