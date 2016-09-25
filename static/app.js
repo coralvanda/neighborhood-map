bc8e58ff29bae87e45f55b28eeccfac1da7a3625
Eric <coralvanda@gmail.com>
2016-09-25 15:00:53 +0200

    Make observable array display on page

--- static/app.js
+++ static/app.js
@@ -2,7 +2,7 @@
 
 var map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: -34.397, lng: 150.644},
-	  zoom: 8
+	  zoom: 10
 });
 
 var input = document.getElementById('pac-input');
@@ -16,19 +16,29 @@ var autocomplete = new google.maps.places.Autocomplete(input);
 
 var myList = [
     {
-    	position: {lat: -34.395, lng: 150.645},
+    	position: {lat: -34.385, lng: 150.645},
     	map: map,
     	title: "A"
     },
     {
-    	position: {lat: -34.398, lng: 150.648},
+    	position: {lat: -34.398, lng: 150.658},
     	map: map,
     	title: "B"
     },
     {
-    	position: {lat: -34.395, lng: 150.648},
+    	position: {lat: -34.4, lng: 150.648},
     	map: map,
     	title: "C"
+    },
+    {
+    	position: {lat: -34.6, lng: 150.848},
+    	map: map,
+    	title: "D"
+    },
+    {
+    	position: {lat: -34.5, lng: 150.540},
+    	map: map,
+    	title: "E"
     }
 ]
 
@@ -39,8 +49,8 @@ var viewModel = {
 	},
 
 	buildList: function(itemList) {
-	var self = this;
-	this.placeList = ko.observableArray(itemList);
+	    var self = this;
+	    this.placeList = ko.observableArray(itemList);
 	}
 }
 
