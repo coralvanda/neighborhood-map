b46d4a52d853d0343c276e85ab568c42c6efba76
Eric <coralvanda@gmail.com>
2016-09-28 15:05:54 +0200

    Finish adding functionality for hamburger button

--- static/app.js
+++ static/app.js
@@ -65,10 +65,13 @@ var view = {
 
     hideNav: function() {
         var sideBar = document.getElementById("side-bar");
+        var mapDisplay = document.getElementById("map");
         if (sideBar.style.display !== 'none') {
             sideBar.style.display = 'none';
+            mapDisplay.style.height = "100%";
         } else {
             sideBar.style.display = 'block';
+            mapDisplay.style.height = "80%";
         }
     }
 };
