e64870d4cf65f8931d2c8834dbbe1c35134aeb25
Eric <coralvanda@gmail.com>
2016-09-30 20:54:58 +0200

    Made a mess trying to get observables working

--- static/app.js
+++ static/app.js
@@ -1,38 +1,55 @@
 // Google Map API key AIzaSyCz4KE50vlj_IUxdao2tVokcSaN8GJIdb4
 
+/*
+// Put the map on the page
 var map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: -34.397, lng: 150.644},
 	  zoom: 10
 });
+*/
 
-var input = document.getElementById('pac-input');
-//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-var autocomplete = new google.maps.places.Autocomplete(input);
+//var input = document.getElementById('pac-input');
+//var autocomplete = new google.maps.places.Autocomplete(input);
+
+ko.bindingHandlers.map = {
+    init: function(element, valueAccessor, allBindingsAccessor, ViewModel) {
+        var value = valueAccessor();
+        var mapOjbect = ko.unwrap(value);
+        mapOjbect.googleMap = new google.maps.Map(element, {
+                center: {
+                    lat: mapOjbect.lat(), 
+                    lng: mapOjbect.lng()
+                },
+                zoom: mapOjbect.zoom
+            }
+        );
+    }
+}
 
 
 
-
+// Make a list of locations to add to the map
 var model = {
     locations: [
         {
         	position: {lat: -34.385, lng: 150.645},
-        	title: "A"
+        	title: "Ancient"
         },
         {
         	position: {lat: -34.398, lng: 150.658},
-        	title: "B"
+        	title: "Bovine"
         },
         {
         	position: {lat: -34.4, lng: 150.648},
-        	title: "C"
+        	title: "Creative"
         },
         {
         	position: {lat: -34.6, lng: 150.848},
-        	title: "D"
+        	title: "Darned"
         },
         {
         	position: {lat: -34.5, lng: 150.540},
-        	title: "E"
+        	title: "Emus"
         }
     ]
 }
@@ -42,10 +59,17 @@ var ViewModel = function() {
     var self = this;
 
     this.placeList = ko.observableArray([]);
+    this.searchTerm = ko.observable("");
+    this.myMap = ko.observable({
+        lat: ko.observable(-34.397), 
+        lng: ko.observable(150.644)
+    });
 
+    // Turn the locations into markers and put them on the map
+    // and populate the ko.observableArray with the markers
     model.locations.forEach(function(place) {
         var marker = new google.maps.Marker(place);
-        marker.setMap(map);
+        marker.setMap(self.myMap().googleMap);
         marker.addListener('click', function() {
             self.selectPlace(this);
             if (marker.getAnimation() !== null) {
@@ -57,14 +81,27 @@ var ViewModel = function() {
         self.placeList.push( new Place(marker) );
     });
 
+    this.searchResults = ko.computed(function() {
+        this.results = ko.observableArray([]);
+        self.placeList().forEach(function(marker) {
+            if (marker.title().indexOf(self.searchTerm) !== -1) {
+                this.results.push(marker)
+                //marker.setMap(myMap);
+            } else {
+                //marker.setMap(null);
+            }
+        });
+        return this.results();
+    });
+
     this.selectedPlace = ko.observable();
 
     this.selectPlace = function(place) {
         self.selectedPlace(place);
     };
 
-    this.checkSelected = ko.computed(function(place) {
-        if (place == self.selectedPlace()) {
+    this.checkSelected = ko.pureComputed(function(place) {
+        if (place === self.selectedPlace()) {
             return true;
         } else {
             return false;
--- static/helper.js
+++ static/helper.js
@@ -1,3 +1,4 @@
+/*
 var hideNav = function() {
     var sideBar = document.getElementById("side-bar");
     var mapDisplay = document.getElementById("map");
@@ -8,7 +9,17 @@ var hideNav = function() {
         sideBar.style.display = 'block';
         mapDisplay.style.height = "80%";
     }
-};
+};*/
 
 var burgerIcon = document.getElementById("burger-icon");
-burgerIcon.addEventListener('click', hideNav);
+burgerIcon.addEventListener('click', function() {
+	var hidden = ko.observable(false);
+	var mapDisplay = document.getElementById("map");
+	if (hidden) {
+		hidden = false;
+		mapDisplay.style.height = "80%";
+	} else {
+		hidden = true;
+		mapDisplay.style.height = "100%";
+	}
+});
